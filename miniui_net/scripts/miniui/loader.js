066b632e18a7232b6b39d0071aa07ba1779bd1b8
fanwang551 <3275849102@qq.com>
2023-07-17 08:48:28 +0000

    add

---
+++ miniui_net/scripts/miniui/loader.js
@@ -0,0 +1,21 @@
+﻿
+if (top.miniuijs) {
+
+    eval(top.miniuijs);
+} else {
+//    document.write('<script src="' + bootPATH + 'miniui/miniui.js" type="text/javascript" ></sc' + 'ript>');
+    
+    $.ajax({
+        url: bootPATH + 'miniui/miniui.js',
+        async: false,
+        dataType: 'text',
+        success: function (text) {
+            window.miniuijs = text;
+            //eval("("+text+")")            
+        }
+    });
+
+    eval(window.miniuijs);    
+}
+
+
